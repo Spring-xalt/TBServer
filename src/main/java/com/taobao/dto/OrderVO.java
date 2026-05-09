7e4ab4b0d8ece184db2f47d7f1cab065e93e6528
Spring <jimifan871@gmail.com>
2026-05-09 22:13:28 +0800

    我的订单模块完成 拿的各方数据

---
+++ src/main/java/com/taobao/dto/OrderVO.java
@@ -0,0 +1,31 @@
+package com.taobao.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/*
+ *@auther:Jimi
+ *@version:1.0
+ *@description:
+ */
+
+@Data
+public class OrderVO {
+    private Integer id;
+    private Integer merchant_id;
+    private Integer consumer_id;
+    private String product_name;
+    private BigDecimal total_amount;
+
+    // 1未支付 2已支付 3已签收
+    private Integer status;
+    private BigDecimal temp_amount;
+    private LocalDateTime receive_time;
+    private LocalDateTime create_time;
+    private LocalDateTime update_time;
+
+    // 商户名称
+    private String merchant_name;
+}
