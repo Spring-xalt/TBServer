6cad65a127a78855eb9b263ec1e0f2f6f74400ee
Spring <jimifan871@gmail.com>
2026-05-20 22:04:29 +0800

    完善文档说明

---
+++ src/test/java/com/taobao/TBServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taobao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TBServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
